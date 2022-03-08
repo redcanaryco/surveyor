5a5a7475d79690186b4d640ad28d4890e5a75eb9
Jonathan Holtmann <1270177+jholtmann@users.noreply.github.com>
2022-03-08 20:02:27 +0000

    load.py documentation improvements and code cleanup

--- load.py
+++ load.py
@@ -1,13 +1,11 @@
 import os
-import re
-from typing import Iterator
+from typing import Iterator, Type
 
 from common import Product
 
 
 # import all files in the 'products' folder
 # this is required so that Product.__subclasses__() can resolve all implemented subclasses
-# this ensure that new subclasses in 'products' will automatically be picked up
 for module in os.listdir(os.path.join(os.path.dirname(__file__), 'products')):
     if module == '__init__.py' or module[-3:] != '.py':
         continue
@@ -15,10 +13,14 @@ for module in os.listdir(os.path.join(os.path.dirname(__file__), 'products')):
     del module
 
 
-def _get_subclasses():
+def _get_subclasses() -> list[Type[Product]]:
+    """
+    Retrieve all subclasses of the "Product" class.
+    """
     seen = set()
 
     for subclass in Product.__subclasses__():
+        # ensure two products don't have the same product string
         if subclass.product in seen:
             raise ValueError(f'Product {subclass.product} is declared multiple times')
 
@@ -30,7 +32,7 @@ def get_product_instance(product: str, **kwargs) -> Product:
     """
     Get an instance of the product implementation matching the specified product string.
     """
-    for subclass in Product.__subclasses__():
+    for subclass in _get_subclasses():
         if subclass.product == product:
             return subclass(**kwargs)
 
