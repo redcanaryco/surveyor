abe1e04a9d7e7e280e2714e00034cdcfb2200ba0
Justin Lentz <jlentz@gosolis.com>
2022-10-05 08:59:54 -0400

    add base_query to self._queries if it exists

--- products/sentinel_one.py
+++ products/sentinel_one.py
@@ -337,7 +337,10 @@ class SentinelOne(Product):
 
     def nested_process_search(self, tag: Tag, criteria: dict, base_query: dict):
         query_base, from_date, to_date = self.build_query(base_query)
-
+        if query_base not in self._queries:
+            if tag not in self._queries:
+                    self._queries[Tag("filter")] = list()
+            self._queries[Tag("filter")].append(Query(from_date, to_date, None, None, None, query_base))
         try:
             for search_field, terms in criteria.items():
                 all_terms = ', '.join(f'"{term}"' for term in terms)
