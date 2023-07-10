fb76b9bc17ae6c0cf9116a3fbd3854b3ff608516
Cori Smith <cori.smith@redcanary.com>
2023-07-10 15:50:38 -0500

    add dfe test coverage for limit ranges

--- tests/test_microsoft_defender_for_endpoints.py
+++ tests/test_microsoft_defender_for_endpoints.py
@@ -8,6 +8,31 @@ sys.path.append(os.getcwd())
 from products.microsoft_defender_for_endpoints import DefenderForEndpoints
 from common import Tag
 
+def test_init_lower_limit_option(tmpdir, mocker):
+    mocker.patch.object(DefenderForEndpoints, '_authenticate')
+    cred_file_path = tmpdir.mkdir('test_dir').join('test_creds.ini')
+    cred_file_path.write("asdfasdfasdf")
+    dfe_product = DefenderForEndpoints(profile='default',creds_file=cred_file_path, limit=-2)
+    assert dfe_product._limit == -1
+
+
+def test_init_upper_limit_option(tmpdir, mocker):
+    mocker.patch.object(DefenderForEndpoints, '_authenticate')
+    cred_file_path = tmpdir.mkdir('test_dir').join('test_creds.ini')
+    cred_file_path.write("asdfasdfasdf")
+    dfe_product = DefenderForEndpoints(profile='default',creds_file=cred_file_path, limit=100001)
+    assert dfe_product._limit == -1
+
+
+def test_init_lower_limit_option(tmpdir, mocker):
+    mocker.patch.object(DefenderForEndpoints, '_authenticate')
+    cred_file_path = tmpdir.mkdir('test_dir').join('test_creds.ini')
+    cred_file_path.write("asdfasdfasdf")
+    dfe_product = DefenderForEndpoints(profile='default',creds_file=cred_file_path, limit=10)
+    assert dfe_product._limit == 10
+
+
+
 @pytest.fixture
 def dfe_product():
     with patch.object(DefenderForEndpoints, "__init__", lambda x, y: None):
