5c45c24bf5790d2b07c4ece0dc9bf71fc780b6c1
Cori Smith <cori.smith@redcanary.com>
2023-10-06 15:09:23 -0500

    first pass on test_surveyor.py

--- tests/test_surveyor.py
+++ tests/test_surveyor.py
@@ -3,21 +3,38 @@ import sys
 import os
 from click.testing import CliRunner
 sys.path.append(os.getcwd())
-from surveyor import cli
+from surveyor import Surveyor
+#from surveyor import cli
 from common import Tag
-
 
 @pytest.fixture
 def runner():
     return CliRunner()
 
 
-def test_survey_cbr(runner: CliRunner, mocker):
+def test_survey_cbr(mocker):
     """
     Verify when passing `cbr` parameter, the CbResponse product is called
     """
     mocked_func = mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
-    runner.invoke(cli, ["cbr"])
+    args = {
+        'query': 'ProcessCmd RegExp “schtasks” AND processName != “Manages scheduled tasks”',
+        'no_file': False,
+        'no_progress': False,
+        'log_dir': 'logs',
+        'output_format': 'csv',
+        'edr': 'cbr',
+        'product_args': {},
+        'creds': {
+            'profile': 'test',
+            'site_ids': [],
+            'account_ids': [],
+            'account_names': [],
+            'creds_file': 'cbr.ini',
+            'bypass': False
+        }
+    }
+    Surveyor().process_telemetry(**args)
     mocked_func.assert_called()
 
 
@@ -166,6 +183,9 @@ def test_invalid_def_file(runner, mocker):
 
 
 def test_invalid_sigma_rule(runner, mocker):
+    """
+    Verify if a non-existent sigma rule file is passed, it is logged and nothing is passed to the EDR product
+    """
     mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     mocked_nested_process_search = mocker.patch('products.vmware_cb_response.CbResponse.nested_process_search')
     result = runner.invoke(cli, ["--sigmarule", "nonexistent.yml"])
@@ -174,6 +194,9 @@ def test_invalid_sigma_rule(runner, mocker):
 
 
 def test_invalid_sigma_dir(runner, mocker):
+    """
+    Verify if a non-existent sigma rule directory is passed, it is logged and nothing is passed to the EDR product
+    """
     mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     mocked_nested_process_search = mocker.patch('products.vmware_cb_response.CbResponse.nested_process_search')
     result = runner.invoke(cli, ["--sigmadir", "./nonexistent_dir"])
@@ -199,7 +222,7 @@ def test_ioc_file(runner, mocker):
 
 def test_ioc_file_with_base_query(runner, mocker):
     """
-    Verify if an IOC file is passed, it is logged and an EDR product is called
+    Verify if an IOC file is passed with a base query, it is logged and an EDR product is called with both values
     """
     mocked_func = mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     mocked_nested_process_search = mocker.patch('products.vmware_cb_response.CbResponse.nested_process_search')
@@ -214,28 +237,10 @@ def test_ioc_file_with_base_query(runner, mocker):
         mocked_nested_process_search.assert_called_once_with(Tag(f'IOC - {ioc_file_path}', 'ioc_list.txt'), {'ipaddr':['127.0.0.1']}, {'days':5, 'hostname':'workstation1', 'username':'admin'})
 
 
-def test_no_argument_provided(runner):
-    arguments = ["--deffile", "--profile", "--prefix", "--output", "--defdir", "--iocfile", "--ioctype", "--query", "--hostname", "--days", "--minutes", "--username", "--limit"]
-
-    for arg in arguments:
-        result = runner.invoke(cli, [arg])
-        assert f"Option '{arg}' requires an argument" in result.output
-        assert result.exit_code != 0
-
-
-def test_unsupported_command(runner):
-    result = runner.invoke(cli, ['asdfasdfasdfasdf'])
-    assert f"Error: No such command 'asdfasdfasdfasdf'" in result.output
-    assert result.exit_code != 0
-
-
-def test_unsupported_option(runner):
-    result = runner.invoke(cli, ['--asdfasdfasdfasdf'])
-    assert f"Error: No such option: --asdfasdfasdfasdf" in result.output
-    assert result.exit_code != 0
-
-
 def test_dependent_ioc_args(runner):
+    """
+    Verify if IOC file is provided and IOC type is not, an error is thrown
+    """
     with runner.isolated_filesystem() as temp_dir:
         ioc_file_path = os.path.join(temp_dir, "ioc_list.txt")
         with open(ioc_file_path, 'w') as deffile:
@@ -247,18 +252,27 @@ def test_dependent_ioc_args(runner):
 
 
 def test_invalid_ioc_file(runner):
+    """
+    Verify IOC file is indeed a file
+    """
     result = runner.invoke(cli, ["--iocfile", "nonexistent.txt", "--ioctype", "md5"])
     assert "Supplied --iocfile is not a file" in result.output
     assert result.exit_code != 0
 
 
 def test_mutually_exclusive_days_mins(runner):
+    """
+    Verify if days and minutes are mutually exclusive
+    """
     result = runner.invoke(cli, ['--days', '3', '--minutes', '4'])
     assert "--days and --minutes are mutually exclusive" in result.output
     assert result.exit_code != 0
 
 
 def test_output_argument_full_path(runner, mocker):
+    """
+    Verify if full path is provided as an output, that is used
+    """
     mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     with runner.isolated_filesystem() as temp_dir:
         full_output_path = os.path.join(temp_dir, "full_output.csv")
@@ -268,6 +282,9 @@ def test_output_argument_full_path(runner, mocker):
 
 
 def test_output_argument_filename(runner, mocker):
+    """
+    Verify if only filename is provded as an output, file is saved in current directory
+    """
     mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     filename_only = 'filename_only.csv'
     full_output_path = os.path.join(os.getcwd(), filename_only)
@@ -277,12 +294,18 @@ def test_output_argument_filename(runner, mocker):
 
 
 def test_mutually_exclusive_output_prefix(runner, mocker):
+    """
+    Verify prefix and output are mutually exclusive
+    """
     mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     result = runner.invoke(cli, ['--prefix', 'test_prefix', '--output', 'test_output.csv'])
     assert "Output arg takes precendence so prefix arg will be ignored" in result.output
 
 
 def test_no_file_output(runner, mocker):
+    """
+    Verify no file is created
+    """
     mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     default_output = 'surveyor.csv'
     runner.invoke(cli, ['--no_file'])
@@ -290,6 +313,9 @@ def test_no_file_output(runner, mocker):
 
 
 def test_base_query_filters_with_query(runner, mocker):
+    """
+    Verify query and base query are both sent to EDR product
+    """
     mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     mocked_process_search = mocker.patch('products.vmware_cb_response.CbResponse.process_search')
     filter_args = ['--days', '5', '--hostname', 'workstation1', '--username', 'admin']
@@ -299,6 +325,9 @@ def test_base_query_filters_with_query(runner, mocker):
 
 
 def test_sigma_rule(runner, mocker):
+    """
+    Verify sigma rule file is passed to the EDR product
+    """
     mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     mocked_nested_process_search = mocker.patch('products.vmware_cb_response.CbResponse.nested_process_search')
     with runner.isolated_filesystem() as temp_dir:
@@ -322,6 +351,9 @@ fields:
 
 
 def test_sigma_rule_with_base_query(runner, mocker):
+    """
+    Verify sigma rule file and base query is passed to the EDR product
+    """
     mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     mocked_nested_process_search = mocker.patch('products.vmware_cb_response.CbResponse.nested_process_search')
     filter_args = ['--days', '5', '--hostname', 'workstation1', '--username', 'admin']
@@ -346,6 +378,9 @@ fields:
 
 
 def test_sigma_dir(runner, mocker):
+    """
+    Verify sigma rule directory is passed to the EDR product
+    """
     mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     mocked_nested_process_search = mocker.patch('products.vmware_cb_response.CbResponse.nested_process_search')
     with runner.isolated_filesystem() as temp_dir:
@@ -387,6 +422,9 @@ fields:
 
 
 def test_sigma_dir_with_base_query(runner, mocker):
+    """
+    Verify sigma rule file and base query are passed to the EDR product
+    """
     mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
     mocked_nested_process_search = mocker.patch('products.vmware_cb_response.CbResponse.nested_process_search')
     filter_args = ['--days', '5', '--hostname', 'workstation1', '--username', 'admin']
@@ -426,51 +464,3 @@ fields:
                           mocker.call(Tag('Test sigma rule 2 - 15ecb82d-b7c0-4e53-9bf3-deedb4c9908c', 'Sigma Rule'), {"query":["process_name:powershell.exe"]}, {'username':'admin', 'hostname':'workstation1','days':5 })]
         assert "Processing sigma rules" in result.output
         mocked_nested_process_search.assert_has_calls(expected_calls, any_order=True)
-
-
-def test_sigma_rule_with_cortex(runner, mocker):
-    mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
-    with runner.isolated_filesystem() as temp_dir:
-        cred_file = os.path.join(temp_dir, "test.ini")
-        with open(cred_file, 'w') as cred_file_output:
-            cred_file_output.write("testing123")
-
-        result = runner.invoke(cli, ['--sigmarule', 'test.yml', 'cortex', '--creds', cred_file])
-        assert 'Neither --sigmarule nor --sigmadir are supported by product "cortex"' in result.output
-        assert result.exit_code != 0
-
-
-def test_sigma_dir_with_cortex(runner, mocker):
-    mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
-    with runner.isolated_filesystem() as temp_dir:
-        cred_file = os.path.join(temp_dir, "test.ini")
-        with open(cred_file, 'w') as cred_file_output:
-            cred_file_output.write("testing123")
-
-        result = runner.invoke(cli, ['--sigmadir', './sigma_dir', 'cortex', '--creds', cred_file])
-        assert 'Neither --sigmarule nor --sigmadir are supported by product "cortex"' in result.output
-        assert result.exit_code != 0
-
-
-def test_sigma_rule_with_s1_pq(runner, mocker):
-    mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
-    with runner.isolated_filesystem() as temp_dir:
-        cred_file = os.path.join(temp_dir, "test.ini")
-        with open(cred_file, 'w') as cred_file_output:
-            cred_file_output.write("testing123")
-
-        result = runner.invoke(cli, ['--sigmarule', 'test.yml', 's1', '--creds', cred_file])
-        assert 'Neither --sigmarule nor --sigmadir are supported by SentinelOne PowerQuery' in result.output
-        assert result.exit_code != 0
-
-
-def test_sigma_dir_with_s1_pq(runner, mocker):
-    mocker.patch('products.vmware_cb_response.CbResponse._authenticate')
-    with runner.isolated_filesystem() as temp_dir:
-        cred_file = os.path.join(temp_dir, "test.ini")
-        with open(cred_file, 'w') as cred_file_output:
-            cred_file_output.write("testing123")
-
-        result = runner.invoke(cli, ['--sigmadir', './sigma_dir', 's1', '--creds', cred_file])
-        assert 'Neither --sigmarule nor --sigmadir are supported by SentinelOne PowerQuery' in result.output
-        assert result.exit_code != 0
